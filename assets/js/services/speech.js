f103400f448f92c0fde19ce90fe1a46de2c1fbe7
Claudiu Teodor Filip <cfilip@opswat.com>
2016-02-20 14:44:32 +0200

    addapted translation

--- assets/js/services/speech.js
+++ assets/js/services/speech.js
@@ -1,47 +1,48 @@
-(function() {
+(function () {
     'use strict';
+
+    window.SpeechRecognition = window.SpeechRecognition ||
+        window.webkitSpeechRecognition ||
+        null;
 
     angular.module('cahoots')
         .service('Speech', function (Translation) {
-        // Test browser support
-        window.SpeechRecognition = window.SpeechRecognition ||
-            window.webkitSpeechRecognition ||
-            null;
+            var self = this;
 
+            self.text = '';
 
-        if (!SpeechRecognition) {
-            return;
-        }
-        var recognizer = new window.SpeechRecognition();
-        var transcription = document.getElementById('subtitle');
-
-
-        recognizer.continuous = true;
-
-        recognizer.onresult = function (event) {
-            var str = '';
-
-            for (var i = event.resultIndex; i < event.results.length; i++) {
-                if (event.results[i].isFinal) {
-                    str = event.results[i][0].transcript;
-                } else {
-                    str += event.results[i][0].transcript;
-                }
+            if (!SpeechRecognition) {
+                return;
             }
 
-            Translation.translate(str, 'ro').then(function (result) {
-                if (result && result.text) {
-                    transcription.textContent = result.text.join();
+            var recognizer = new window.SpeechRecognition();
+            recognizer.continuous = true;
+
+            recognizer.onresult = function (event) {
+                var str = '';
+
+                for (var i = event.resultIndex; i < event.results.length; i++) {
+                    if (event.results[i].isFinal) {
+                        str = event.results[i][0].transcript;
+                    } else {
+                        str += event.results[i][0].transcript;
+                    }
                 }
-            })
 
-        };
+                Translation.translate(str, 'ro').then(function (result) {
+                    if (result && result.text) {
+                        self.text = result.text.join();
+                    }
+                })
 
-        recognizer.start();
+            };
 
-        // Listen for errors
-        recognizer.onerror = function (event) {
-            console.log('Recognition error: ' + event.message);
-        };
-    });
+            recognizer.start();
+
+            // Listen for errors
+            recognizer.onerror = function (event) {
+                console.log('Recognition error: ' + event.message);
+            };
+
+        });
 })();
