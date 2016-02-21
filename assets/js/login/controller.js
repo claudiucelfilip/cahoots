9b17bff04d3234e6e30e3a56b09c2ebfd2ad0cdc
Andrei Demian <ademian@opswat.com>
2016-02-21 15:36:24 +0200

    Update user management

--- assets/js/login/controller.js
+++ assets/js/login/controller.js
@@ -2,32 +2,43 @@
     'use strict';
 
     app.controller('LoginCtrl',
-        function($scope, $state, $localStorage, Api, Error) {
+        function($scope, $state, $stateParams, $localStorage, Api, Error) {
 
             if(typeof($localStorage.userName) !== 'undefined') {
-                $scope.userName = $localStorage.userName;
+                $scope.userName = $localStorage.userName.name;
             }
 
-            if(typeof($localStorage.rooms) !== 'undefined') {
-                $scope.recentRooms = $localStorage.rooms.slice(Math.max($localStorage.rooms.length - 1, 1));
+            if(typeof($localStorage.recentRoom) !== 'undefined') {
+                $scope.recentRoom = $localStorage.recentRoom;
             }
 
+            $scope.error = $stateParams.errorExists;
+            $scope.requestUser = $stateParams.requestUser;
+            $scope.room = $stateParams.roomId;
+
+            $scope.joinRoom = function() {
+                Api.addUser({ second: $stateParams.roomId, name: $scope.user },
+                    function (response) {
+                        $localStorage.userName = response.users[response.users.length - 1];
+                        $localStorage.recentRoom = response;
+
+                        $state.go('meeting', { roomId: response.name, justCreated: true });
+                    },
+                    function (error) {
+                        $scope.error = Error.handler(error);
+                    }
+                );
+            };
+
+            // Regular login (create room)
             $scope.createRoom = function() {
-
 
                 Api.createRoom({ name: $scope.room, user_name: $scope.userName },
                     function(response) {
-                        $localStorage.userName = $scope.name;
-                        $localStorage.authToken = response.auth_token;
+                        $localStorage.userName = response.users[0];
+                        $localStorage.recentRoom = response;
 
-                        if(typeof($localStorage.rooms) === 'undefined') {
-                            $localStorage.rooms = [response.name];
-                        } else {
-                            $localStorage.rooms.push(response.name);
-                        }
-
-                        $state.go('meeting', { roomId: response.name });
-
+                        $state.go('meeting', { roomId: response.name, justCreated: true });
                     },
                     function(error) {
                         $scope.error = Error.handler(error);
